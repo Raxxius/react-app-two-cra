2e8c091c1796406192de0bc89ff7ac085ea3bb8d
Raxxius <raxxman@gmail.com>
2024-01-25 19:42:33 +0000

    added event listener

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,30 @@ import "./App.css";
 function App() {
   //UseState
   const [colourIndex, setColourIndex] = useState(0);
-  const [colours, setColours] = useState(null)
+  const [colours, setColours] = useState(null);
 
   useEffect(() => {
-    console.log(sessionStorage)
+    console.log(sessionStorage);
+    setColours(sessionStorage.coloursTable);
 
-  }, [])
+    // handleStorageChange function
+    const handleStorageChange = () => {
+      setColours(sessionStorage.coloursTable);
+    };
+
+    // Event listener
+    window.addEventListener("storage", handleStorageChange);
+    console.log("event listener engaged");
+
+    // Cleanup
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
 
   return (
     <div className="App">
-      <button></button>
+      <button>cycle colours</button>
     </div>
   );
 }
